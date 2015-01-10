31441a92cd4ac1571dce0e543a7fc28ee34bb098
ipavl <ipavl@users.sourceforge.net>
2015-01-10 17:29:08 -0500

    Initial commit
    Blank Lein template with domain resources folder

---
+++ README.md
@@ -0,0 +1,15 @@
+# swot-clj
+
+A Clojure port of [Lee Reilly](https://github.com/leereilly)'s [Swot library](https://github.com/leereilly/swot)
+to validate email addresses and domains of academic institutions.
+
+## Usage
+
+FIXME
+
+## License
+
+Copyright © 2015 ipavl
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject swot-clj "0.1.0-SNAPSHOT"
+  :description "Clojure port of Lee Reilly's 'Swot' library to validate academic institution email addresses and domains."
+  :url "https://github.com/ipavl/swot-clj"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]])
