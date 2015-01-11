6425e3287b87b9b48c0d26e7dd51f4bd6001e68f
ipavl <ipavl@users.sourceforge.net>
2015-01-10 21:15:59 -0500

    Add basic tests

--- test/swot_clj/core_test.clj
+++ test/swot_clj/core_test.clj
@@ -2,6 +2,16 @@
   (:require [clojure.test :refer :all]
             [swot-clj.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-regex
+  (testing "basic URL"
+    (is (= "stanford.edu" (get-domain "stanford.edu"))))
+  (testing "full URL"
+    (is (= "www.stanford.edu" (get-domain "http://www.stanford.edu"))))
+  (testing "basic email"
+    (is (= "example.com" (get-domain "webmaster@example.com"))))
+  (testing "email with subdomain"
+    (is (= "sub.example.com" (get-domain "webmaster@sub.example.com")))))
+
+(deftest test-domains
+  (testing "blacklisted email"
+    (is (= (is-academic? "mail@australia.edu") false))))
