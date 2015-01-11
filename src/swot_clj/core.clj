8ba270b8ce8568fec239f095da4af08857484205
ipavl <ipavl@users.sourceforge.net>
2015-01-10 20:46:13 -0500

    Add blacklisted domains and whitelisted TLDs, and verify them
    For is-academic?,
    - A blacklisted domain will return false
    - A whitelisted TLD (*not domain + TLD*) will return true
    - Everything else will return nil, including domain.whitelisted.tld for the moment

--- src/swot_clj/core.clj
+++ src/swot_clj/core.clj
@@ -1,6 +1,31 @@
 (ns swot-clj.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn- read-list
+  "Reads each line in a file and returns a vector containing each line."
+  [file]
+  (clojure.string/split-lines (slurp file)))
+
+(defn- in?
+  "Determines if an element is in a given sequence."
+  [seq elm]
+  (some #(= elm %) seq))
+
+(defn get-domain
+  "Strips a string, such as a URL or e-mail address, down to its domain."
+  [text]
+  (clojure.string/lower-case (re-find #"[^@\/:]+[:\d]*$" text)))
+
+(defn is-academic?
+  "Determines if the passed string is an email or domain belonging to an academic institution."
+  [text]
+  (if (nil?
+        (in?
+          (read-list
+            (.getPath (clojure.java.io/resource "blacklist.txt")))
+          (clojure.string/lower-case (get-domain text))))
+    (if nil?
+      (in?
+        (read-list
+          (.getPath (clojure.java.io/resource "whitelist.txt")))
+        (clojure.string/lower-case (get-domain text))))
+    false))
