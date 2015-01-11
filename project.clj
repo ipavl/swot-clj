a3003dc5c15ee06349ca872a84ef7d4137ce197b
ipavl <ipavl@users.sourceforge.net>
2015-01-10 20:45:56 -0500

    Set default REPL namespace

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "https://github.com/ipavl/swot-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :repl-options {:init-ns swot-clj.core})
