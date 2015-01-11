e2967a43a7787352cf12cb6e3946ca72ca1715ba
ipavl <ipavl@users.sourceforge.net>
2015-01-11 00:06:57 -0500

    Update project metadata for Clojars

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
-(defproject swot-clj "0.1.0-SNAPSHOT"
+(defproject swot-clj "0.5.0"
   :description "Clojure port of Lee Reilly's 'Swot' library to validate academic institution email addresses and domains."
   :url "https://github.com/ipavl/swot-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
-  :repl-options {:init-ns swot-clj.core})
+  :repl-options {:init-ns swot-clj.core}
+  :scm {:name "git"
+        :url "https://github.com/ipavl/swot-clj"})
